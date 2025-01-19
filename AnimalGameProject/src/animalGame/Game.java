612e26d51a688e13bdcfdef5bff36aef97e9ebd2
Nathan Barran <nathan.barran@student.tdsb.on.ca>
2025-01-18 22:11:20 -0500

    idk what these changes are

--- AnimalGameProject/src/animalGame/Game.java
+++ AnimalGameProject/src/animalGame/Game.java
@@ -1,17 +1,26 @@
 package animalGame;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Game extends Application {
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage primaryStage) throws Exception {
+		showMenu(primaryStage);		
 	}
 	public static void main(String[] args) {
 		launch(args);
 	}
+	public void showMenu(Stage primaryStage) {
+		Button lionButton = new Button("Lion");
+		VBox buttons = new VBox(lionButton);
 
+		Scene menu = new Scene(buttons, 300,400);
+		primaryStage.setScene(menu);
+		primaryStage.show();
+	}
 }
